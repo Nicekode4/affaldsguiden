ab636d325300833b870466b0d4ea55dd124e9976
Philip Thomsen <philip-th@live.dk>
2023-09-18 09:29:13 +0200

    Supabase credentials
    Added the correct supabase crendtials to my supabase.js file

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://tqdmywgvevlkngjvxkcy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZG15d2d2ZXZsa25nanZ4a2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MTQ1ODgsImV4cCI6MjAwOTM5MDU4OH0.HNfaquToN5syGbz7m17-rf1FfYDeH7pquYexmGySt00';
+const supabaseUrl = 'https://haqfhdjzvotveqhuioyh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhcWZoZGp6dm90dmVxaHVpb3loIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUwMTk3MDEsImV4cCI6MjAxMDU5NTcwMX0.rQMcPyPuixH6lH2blttKpb9kHUzantnTYaveJFfCCIE';
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
