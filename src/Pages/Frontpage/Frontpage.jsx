cfbe9d9db9f21c20d4d73b29d2d207b17d33a138
Philip Thomsen <philip-th@live.dk>
2023-09-18 14:53:07 +0200

    Frontpage - mobile version
    Finished the generel style of the frontpage for the mobile version

--- src/Pages/Frontpage/Frontpage.jsx
+++ src/Pages/Frontpage/Frontpage.jsx
@@ -4,7 +4,35 @@ import { FrontpageStyle } from './Frontpage.style'
 function Frontpage() {
   return (
     <FrontpageStyle>
-            <div>Frontpage</div>
+      <section className='CTASec'>
+<h1>Find og anmeld genbrugsstationer</h1>
+<article>
+  <button>Find station</button>
+  <button>Login</button>
+</article>
+      </section>
+
+      
+            <section className='textSec'>              
+              <img src={require('../../Images/frontpageImg1.png')} alt="Image of trashcan" />
+              <article>
+                <h2>Din guide til sortering</h2>
+                <p>Her kan du se hvordan du skal sortere og hvad der skal i hvilke beholdere. Du får også tips og tricks til, hvordan du gør det nemt at sortere hjemme hos dig.</p>
+                <button>Se affaldsguide</button>
+              </article>
+
+            </section>
+
+            <section className='textSec'>              
+              <img src={require('../../Images/FrontpageImg2.png')} alt="Image of trashcan" />
+              <article>
+                <h2>Bestil din nye affaldsbeholder</h2>
+                <p>when an unknown printer took a galley of type and scramble it to make a type specimen book. It has survived not only</p>
+                <button>Bestil nu</button>
+              </article>
+
+            </section>
+            <img src={require('../../Images/Layout/bg-wave-3.svg')} alt="Wave" />
     </FrontpageStyle>
   )
 }
--- src/Pages/Frontpage/Frontpage.style.js
+++ src/Pages/Frontpage/Frontpage.style.js
@@ -1,5 +1,66 @@
 import styled from "styled-components";
 
-export const FrontpageStyle = styled.main`
+export const FrontpageStyle = styled.section`
+margin-left: 2vw;
+margin-right: 2vw;
+button{
+    color: white;
+    background-color: #114D46;
+    border: none;
+    padding: 2vh 5vw;
+    border-radius: 6px;
+}
+
+.CTASec{
+    margin-top: 2vh;
+    margin-bottom: 2vh;
+    margin-right: 2vh;
+    background-color: #D8EADB;
+    border-radius: 6px;
+    padding: 2vh 20vw;
+    h1{
+        color: #222A41;
+        text-align: center;
+        margin-bottom: 3vh;
+    }
+    button{
+    color: white;
+    background-color: #114D46;
+    border: none;
+    padding: 2vh 5vw;
+    border-radius: 6px;
+    margin-left: 1vw;
+}
+
+}
+
+.textSec{
+    text-align: center;
+   h2{
+    color: #222A41;
+    margin-bottom: 2vh;
+    margin-top: 2vh;
+}
+
+p{
+    color: #222A41;
+    margin-bottom: 2vh;
+    margin-top: 2vh;
+}
+
+button{
+    color: white;
+    background-color: #114D46;
+    border: none;
+    padding: 2vh 5vw;
+    border-radius: 6px;
+    margin-bottom: 2vh;
+    margin-top: 2vh;
+}
+
+img{
+    width: 100%;
+} 
+}
 
 `
