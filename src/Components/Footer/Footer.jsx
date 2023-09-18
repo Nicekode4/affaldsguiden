31a926f65ce938fc83df1c684bc2974293eb0ce5
Philip Thomsen <philip-th@live.dk>
2023-09-18 15:13:02 +0200

    Footer - mobile version
    Created and styled the footer for the mobile version

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,14 @@ import { FooterStyle } from './Footer.style'
 function Footer() {
   return (
     <FooterStyle>
-            <div>Footer</div>
+      <div className='logo'>
+<img src={require('../../Images/whiteLogo.png')} alt="logo" />            
+<p>Affaldsguiden</p>
+      </div>
+
+            <p className='text'>Vi arbejder for at informere om korrekt affaldssortering. Ved at sortere hjælper du os, men også klimaet.</p>
+            <p className='copyright'>©2023 Affaldsguiden. </p>
+            <p className='toTop'>Til toppen <img src="" alt="back to top" /></p>
     </FooterStyle>
   )
 }
--- src/Components/Footer/Footer.style.js
+++ src/Components/Footer/Footer.style.js
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
 export const FooterStyle = styled.footer`
+height: auto;
+background-color: #114D46;
+color: white;
+text-align: center;
 
+p{
+    padding: 2vh 0vw;
+}
+
+.logo{
+    padding: 2vh 0vw;
+    padding-left: 30%;
+    display: flex;
+    img{
+        margin-right: 2vw; 
+        height  :6vh ;
+    }
+
+    p{
+        font-weight: bold;
+    }
+}
+.text{
+
+}
+.copyright{
+
+}
+.toTop{
+
+}
 `
