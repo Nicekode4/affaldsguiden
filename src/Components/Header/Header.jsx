042166ca40abe5655a91fb5387663dd4c9ef4f20
Philip Thomsen <philip-th@live.dk>
2023-09-19 09:55:33 +0200

    Better quality logo
    replaced the png logo on teh header with a better quality svg logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { HeaderStyle } from './Header.style'
 import Navbar from '../Navbar/Navbar'
+import logo from '../../Images/Layout/logo.svg'
 
 function Header() {
   return (
     <HeaderStyle>
       <div>
-           <img src={require('../../Images/logo.png')} alt="Logo" />  
+           <img src={logo} alt="Logo" />  
            <p>Affaldsguiden</p> 
       </div>
 
