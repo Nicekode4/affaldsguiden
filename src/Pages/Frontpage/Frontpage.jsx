0e621e0e3753d8c081a6da39fd72bc0811bd43ad
Philip Thomsen <philip-th@live.dk>
2023-09-20 12:17:15 +0200

    Styling front page and slider
    Making the desktop responsive and starting on the slider

--- src/Pages/Frontpage/Frontpage.jsx
+++ src/Pages/Frontpage/Frontpage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FrontpageStyle } from './Frontpage.style'
+import slide3 from '../../Images/Slideshow/affald-skov-1.jpg'
+import slide2 from '../../Images/Slideshow/affald-strand-2.jpg'
+import slide1 from '../../Images/Slideshow/malerspande.jpg'
 
 function Frontpage() {
+  const imgArray = [
+    slide1,
+    slide2,
+    slide3
+  ]
+  const [slideNum, setSlideNum] = useState(0)
+  if (slideNum > imgArray.length - 1) {
+    setSlideNum(0)
+  }
   return (
     <FrontpageStyle>
+      <section style={{backgroundImage: `url(${imgArray[slideNum]})`}} className='sliderSec'>
+      <button onClick={() => setSlideNum(prev => prev + 1)}>Next</button>
+      <div></div>
+      <button onClick={() => setSlideNum(slideNum + 1)}>Prev</button>
+      </section>
       <section className='CTASec'>
 <h1>Find og anmeld genbrugsstationer</h1>
 <article>
@@ -14,25 +31,25 @@ function Frontpage() {
 
       
             <section className='textSec'>              
-              <img src={require('../../Images/frontpageImg1.png')} alt="Image of trashcan" />
+              <img className='mobileImg' src={require('../../Images/frontpageImg1.png')} alt="Image of trashcan" />
               <article>
-                <h2>Din <strong>guide til sortering</strong> </h2>
+                <h2>Din <strong>guide</strong> <strong>til</strong> <strong>sortering</strong></h2>
                 <p>Her kan du se hvordan du skal sortere og hvad der skal i hvilke beholdere. Du får også tips og tricks til, hvordan du gør det nemt at sortere hjemme hos dig.</p>
                 <button>Se affaldsguide</button>
               </article>
-
+              <img className='desktopImg' src={require('../../Images/frontpageImg1.png')} alt="Image of trashcan" />
             </section>
 
             <section className='textSec'>              
               <img src={require('../../Images/FrontpageImg2.png')} alt="Image of trashcan" />
               <article>
-                <h2>Bestil <strong>din nye affaldsbeholder</strong></h2>
+                <h2>Bestil <strong>din</strong> <strong>nye</strong> <strong>affaldsbeholder</strong></h2>
                 <p>when an unknown printer took a galley of type and scramble it to make a type specimen book. It has survived not only</p>
                 <button>Bestil nu</button>
               </article>
 
             </section>
-            <img src={require('../../Images/Layout/bg-wave-3.svg')} alt="Wave" />
+            <img className='desktopImg' src={require('../../Images/Layout/bg-wave-3.svg')} alt="Wave" />
     </FrontpageStyle>
   )
 }
--- src/Pages/Frontpage/Frontpage.style.js
+++ src/Pages/Frontpage/Frontpage.style.js
@@ -33,7 +33,12 @@ button{
 }
 
 }
-
+.desktopImg{
+    display: none;
+}
+.mobileImg{
+    display: block;
+}
 .textSec{
     text-align: center;
     strong {
@@ -79,6 +84,118 @@ button{
 img{
     width: 100%;
 } 
+}
+.sliderSec{
+    display: none;
+}
+@media screen and (min-width: 768px) {
+    .sliderSec{
+        display: grid;
+        grid-template-columns: 10% 80% 10%;
+        width: 100%;
+            height: 60vh;
+        background-size: cover;
+        margin: 0;
+        padding: 0;
+        button{
+    color: white;
+    background-color: none;
+    border: none;
+    padding: 2vh 5vw;
+    border-radius: 6px;
+}
+    }
+
+
+.CTASec{
+    text-align: center;
+    margin-top: 2vh;
+    margin-bottom: 2vh;
+    margin-left: 30%;
+    background-color: #D8EADB;
+    border-radius: 6px;
+    padding: 2vh 2vw;
+    width: 35%;
+    article{
+        width: max-content;
+         button{
+    color: white;
+    background-color: #114D46;
+    border: none;
+    padding: 2vh 5vw;
+    border-radius: 6px;
+    margin-left: 1vw;
+}   
+    }
+    h1{
+        color: #222A41;
+        text-align: center;
+        margin-bottom: 3vh;
+    }
+
+
+}
+.mobileImg{
+    display: none;
+}
+
+.desktopImg{
+    display: block;
+}
+.textSec{
+    text-align: start;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20vh;
+    strong {
+        display: inline-block;
+      position: relative;
+      font-weight: 400;
+  }
+
+  strong::before {
+      /* Highlight color */
+      background-color: #62BB65;
+      opacity: 50%;
+
+      content: "";
+      position: absolute;
+      width: calc(100% + 20px);
+      height: 20%;
+      left: -2px;
+      bottom: 25px;
+      z-index: -1;
+  }
+  
+   h2{
+    color: #222A41;
+    margin-bottom: 2vh;
+    margin-top: 2vh;
+    font-size: 74px;
+    font-weight: 400;
+}
+
+p{
+    color: #222A41;
+    margin-bottom: 2vh;
+    margin-top: 2vh;
+}
+
+button{
+    color: white;
+    background-color: #114D46;
+    border: none;
+    padding: 2vh 5vw;
+    border-radius: 6px;
+    margin-bottom: 2vh;
+    margin-top: 2vh;
+}
+
+img{
+    width: 100%;
+} 
+
+}
 }
 
 `
