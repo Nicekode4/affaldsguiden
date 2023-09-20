44499575faf058bd601aa1a26a9b8249d3d85084
Philip Thomsen <philip-th@live.dk>
2023-09-20 13:46:15 +0200

    Styling of station overview
    Finished styling teh station overview page

--- src/Components/StationsCard/StationCard.style.js
+++ src/Components/StationsCard/StationCard.style.js
@@ -1,11 +1,42 @@
 import { styled } from "styled-components";
 
 export const StationCardStyle = styled.article`
+box-shadow: -1px 2px 15px 0px rgba(0,0,0,0.41);
+border-radius: 8px;
 margin: 2vh 0vw;
 iframe{
     border: none;
-    height: 50vh;
+    height: 30vh;
 width: auto;
+}
+
+.title{
+    color: black;
+    font-size: 16px;
+    padding-left: 1vw;
+    width: 90%;
+}
+
+.text{
+    color: black;
+    opacity: 50%;
+    font-size: 14px;
+    padding-left: 1vw;
+}
+
+.rating{
+    display: grid;
+    grid-template-columns: 50% 50%;
+    margin-top: 10vh;
+    padding-left: 1vw;
+}
+
+@media screen and (min-width: 768px) {
+    margin-bottom: 10vh;
+    iframe{
+    border: none;
+    height: 30vh;
+width: 100%;
 }
 
 .title{
@@ -23,5 +54,6 @@ width: auto;
     display: grid;
     grid-template-columns: 50% 50%;
     margin-top: 10vh;
+}   
 }
 `
--- src/Pages/Stations/Stations.style.js
+++ src/Pages/Stations/Stations.style.js
@@ -4,4 +4,13 @@ export const StationsStyle = styled.section`
 display: grid;
 grid-template-columns: repeat(1, 1fr);
 justify-items: center;
+
+@media screen and (min-width: 768px) {
+    margin: 0vh 2vw;
+    margin-top: 20vh;
+    margin-bottom: 20vh;
+    display: grid;
+grid-template-columns: repeat(3, 1fr);
+justify-items: center;   
+}
 `
