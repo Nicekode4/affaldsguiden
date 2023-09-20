3120ed0e30e6b4872dd1c5d6eb09cdf502619cac
Philip Thomsen <philip-th@live.dk>
2023-09-20 12:44:11 +0200

    Desktop styling
    Done styling the overview of the trashguide

--- src/Components/TrashCard/TrashCard.style.js
+++ src/Components/TrashCard/TrashCard.style.js
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 
 export const TrashCardStyle = styled.article`
 background-color: ${props => props.color};
-width: fit-content;
+width: 90%;
 margin-left: 5%;
 margin-bottom: 2vh;
 border-radius: 6px;
@@ -10,11 +10,29 @@ text-align: center;
 color: white;
 
 img{
-    height: 50vh;
+    height: 40vh;
     width: 100%;
 }
 
 h2{
     padding: 1vh 1vw;
+}
+
+@media screen and (min-width: 768px) {
+    width: fit-content;
+margin-left: 5%;
+margin-bottom: 2vh;
+border-radius: 6px;
+text-align: center;
+color: white;
+
+img{
+    height: 40vh;
+    width: 100%;
+}
+
+h2{
+    padding: 1vh 1vw;
+}   
 }
 `
--- src/Pages/Trashguide/Trashguide.style.js
+++ src/Pages/Trashguide/Trashguide.style.js
@@ -47,4 +47,60 @@ margin-left: 10%;
     width: auto;
     }
 }
+
+@media screen and (min-width: 768px) {
+    h1{
+    font-size: 30px;
+    margin-top: 5vh;
+    color: #0E4F2D;
+    text-align: center;
+    span{
+        display: block;
+        color: #119B1E;
+    }
+}
+
+.searchSec{
+width: 45%;
+box-shadow: -1px 9px 35px 0px rgba(0,0,0,0.41);
+margin: 2vh 0vw;
+margin-left: 25%;
+margin-bottom: 20vh;
+    border-radius: 50px;
+    padding: 2vh 2vw;
+    input{
+        border: none;
+        padding-left: 2vw;
+        width: 35vw;
+    }
+    input::placeholder{
+        color: black;
+    }
+
+    button{
+        border: none;
+    }
+}
+
+.titleArt{
+    display: grid;
+    grid-template-columns: 75% 30%;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    h2{
+        color: white;
+        padding: 5vh 7vw;
+    }
+
+    img{
+        height: 15vh;
+    width: auto;
+    }
+}
+.categorySec{
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+margin-bottom: 40vh;
+}
+}
 `
