cba8ddad8e2732938740a15192e1fa014e3a4605
Philip Thomsen <philip-th@live.dk>
2023-09-20 16:28:43 +0200

    Added .env
    Added teh .env file and added the database credentials to it for better security

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://haqfhdjzvotveqhuioyh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhcWZoZGp6dm90dmVxaHVpb3loIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUwMTk3MDEsImV4cCI6MjAxMDU5NTcwMX0.rQMcPyPuixH6lH2blttKpb9kHUzantnTYaveJFfCCIE';
+const supabaseUrl = process.env.REACT_APP_URL
+const supabaseKey = process.env.REACT_APP_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
