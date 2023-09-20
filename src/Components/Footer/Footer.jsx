4855fdb919812c103be89a547aa5d272f8e6033a
Philip Thomsen <philip-th@live.dk>
2023-09-20 17:04:53 +0200

    Footer desktop version
    Styling for teh footer desktop version

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,13 +4,16 @@ import { FooterStyle } from './Footer.style'
 function Footer() {
   return (
     <FooterStyle>
-      <div className='logo'>
+      <div>
+              <div className='logo'>
 <img src={require('../../Images/whiteLogo.png')} alt="logo" />            
 <p>Affaldsguiden</p>
       </div>
 
             <p className='text'>Vi arbejder for at informere om korrekt affaldssortering. Ved at sortere hjælper du os, men også klimaet.</p>
             <p className='copyright'>©2023 Affaldsguiden. </p>
+      </div>
+
             <p className='toTop'>Til toppen <img src="" alt="back to top" /></p>
     </FooterStyle>
   )
--- src/Components/Footer/Footer.style.js
+++ src/Components/Footer/Footer.style.js
@@ -32,4 +32,42 @@ p{
 .toTop{
 
 }
+
+@media screen and (min-width: 768px) {
+    height: auto;
+background-color: #114D46;
+color: white;
+text-align: start;
+padding-left: 5%;
+display: grid;
+grid-template-columns: 80% 30%;
+
+p{
+    padding: 2vh 0vw;
+    width: 50%;
+}
+
+.logo{
+    padding: 2vh 0vw;
+    display: flex;
+    img{
+        margin-right: 2vw; 
+        height  :6vh ;
+    }
+
+    p{
+        font-weight: bold;
+    }
+}
+.text{
+
+}
+.copyright{
+
+}
+.toTop{
+text-align: end;
+margin-top: 21vh;
+}   
+}
 `
